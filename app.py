5415111c2df23f6f1a6285019db10c5338cedb51
PurneshN <95577013+PurneshN@users.noreply.github.com>
2024-01-01 19:41:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
 def home_page(name=None):
-    return render_template('Index.html',name=name)
+    return render_template('index.html',name=name)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5001)
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000)
